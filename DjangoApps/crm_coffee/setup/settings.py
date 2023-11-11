ca5a371a84739b51f5991f0f0c3bc352c5d97243
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2023-11-11 17:44:05 -0300

    Criando autenticação e páginas de login e cadastro

--- DjangoApps/crm_coffee/setup/settings.py
+++ DjangoApps/crm_coffee/setup/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,10 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+LOGIN_REDIRECT_URL = '/dashboard/'
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'index'
 
 
 # Application definition
--- DjangoApps/crm_coffee/setup/urls.py
+++ DjangoApps/crm_coffee/setup/urls.py
@@ -1,10 +1,15 @@
+from os import name
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views
 from core.views import index, about
+from userprofile.views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('about/', about, name="about")
+    path('about/', about, name="about"),
+    path('signup/', signup, name="signup"),
+    path('login/', views.LoginView.as_view(template_name='userprofile'), name="login"),
+    path('logout/', views.LogoutView.as_view(), name="logout")
 ]
