f0a8584960b8510a2c368a647f2a4a93089e3d75
Ayko Sousa do Nascimento <ayko.souza74@gmail.com>
2023-11-28 22:23:25 -0300

    Criando filtros para os posts

--- DjangoApps/crm_coffee/userprofile/models.py
+++ DjangoApps/crm_coffee/userprofile/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, related_query_name='userprofile', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
--- DjangoApps/my-blog/blog/models.py
+++ DjangoApps/my-blog/blog/models.py
@@ -11,13 +11,13 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    create_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     update_on = models.DateTimeField(auto_now=True)
     slug = models.CharField(max_length=200, unique=True)
     status = models.IntegerField(choices=STATUS, default=0)
     
     class Meta:
-        ordering = ['create_on']
+        ordering = ['created_on']
     
     def __str__(self):
         return self.title
